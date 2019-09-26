14eefb3ae053d2fda9f14e49e15aa146b9268b88
mkki <gosorn1@gmail.com>
2019-09-26 22:26:14 +0900

    feat: Add stores

---
+++ KimMinkang1902/todo-app-client/src/stores/folder.ts
@@ -0,0 +1,40 @@
+import { observable, action } from "mobx";
+import RootStore from "./index";
+import { Folder } from "../models";
+
+class FolderStore {
+  readonly root: RootStore;
+  @observable public folders: Array<Folder> = [];
+
+  constructor(root: RootStore) {
+    this.root = root;
+  }
+
+  @action
+  initialize = (folders: Array<Folder>) => {
+    this.folders = folders;
+  };
+
+  @action
+  newFolder = (item: Folder) => {
+    this.folders.push(new Folder(item.id, item.title));
+  };
+
+  @action
+  editFolder = (id: string, title: string) => {
+    this.folders = this.folders.map(folder => {
+      if (folder.id === id) {
+        folder.title = title;
+      }
+
+      return folder;
+    });
+  };
+
+  @action
+  deleteFolder = (id: string) => {
+    this.folders = this.folders.filter(folder => folder.id !== id);
+  };
+}
+
+export default FolderStore;
