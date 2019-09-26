69797895cf1a56c93feb9db7e8a3ff38abd198ef
mkki <gosorn1@gmail.com>
2019-09-26 22:26:42 +0900

    feat: Add models

---
+++ KimMinkang1902/todo-app-client/src/models/Todo.ts
@@ -0,0 +1,19 @@
+export const StatType = {
+  ACTIVE: "active",
+  DONE: "done",
+  BOOKMARKED: "bookmarked"
+};
+
+export default class Todo {
+  public id: string;
+  public text: string;
+  public stat: string;
+  public folderId: string;
+
+  constructor(id: string, text: string, folderId: string, stat: string = "active") {
+    this.id = id;
+    this.text = text;
+    this.stat = stat;
+    this.folderId = folderId;
+  }
+}
